5cb2da52455857a92c2bf10f249b3a685563428d
veeracs <veeracs@gmail.com>
2018-10-13 20:36:58 -0400

    Add React

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,15 @@
-console.log('Minimal setup...');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div className="home">
+    <h1>Minimal setup - React, Babel, Webpack</h1>
+  </div>
+);
+
+const title = 'Minimal setup - React, Babel, Webpack';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
