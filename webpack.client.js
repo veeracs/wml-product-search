acebcfd8eae7ad6d90bacb273a76ed277f56873b
veeracs <veeracs@gmail.com>
2018-10-13 20:02:40 -0400

    setup babel configuration

--- webpack.client.js
+++ webpack.client.js
@@ -1,11 +1,23 @@
 module.exports = {
   //  tell webpack, the entry point of server application
   entry: './src/client/index.js',
-  
+
   //  tell webpack, where to put the output file generated
   output: {
     path: __dirname + 'dist',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
   },
   devServer: {
         publicPath: '/',
